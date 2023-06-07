f3cac45326994d9e57b4375cadee567da2ffcb86
Jawny-E <egrov.johnsen@gmail.com>
2023-06-07 14:06:52 +0200

    github

---
+++ Courses/Beejs Guide to C Programming/Chapter1Examples.C
@@ -0,0 +1,11 @@
+/*
+    Basic Libraries included in the example code
+*/
+#include <stdio.h>
+
+/*
+    Simple start here with the all time classic
+*/
+int main(){
+    printf("Hello World!\n");
+}
